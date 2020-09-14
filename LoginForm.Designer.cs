21910eaaaf4520ead53f40507987f4ef039a3e3f
akbj44 <jenaamit202@gmail.com>
2020-09-14 11:52:18 +0530

    First Commit

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace RegistrationForm
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.showPasswordCheckBox = new System.Windows.Forms.CheckBox();
+            this.openRegisterFormLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(268, 177);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(129, 46);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Log-in";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(92, 23);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(130, 29);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Username:";
+            this.usernameLabel.Click += new System.EventHandler(this.usernameLabel_Click);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTextBox.Location = new System.Drawing.Point(268, 20);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(356, 34);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // showPasswordCheckBox
+            // 
+            this.showPasswordCheckBox.AutoSize = true;
+            this.showPasswordCheckBox.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showPasswordCheckBox.Location = new System.Drawing.Point(460, 132);
+            this.showPasswordCheckBox.Name = "showPasswordCheckBox";
+            this.showPasswordCheckBox.Size = new System.Drawing.Size(164, 27);
+            this.showPasswordCheckBox.TabIndex = 3;
+            this.showPasswordCheckBox.Text = "Show Password";
+            this.showPasswordCheckBox.UseVisualStyleBackColor = true;
+            this.showPasswordCheckBox.CheckedChanged += new System.EventHandler(this.ShowPasswordCheckBox_CheckedChanged);
+            // 
+            // openRegisterFormLinkLabel
+            // 
+            this.openRegisterFormLinkLabel.AutoSize = true;
+            this.openRegisterFormLinkLabel.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openRegisterFormLinkLabel.Location = new System.Drawing.Point(92, 245);
+            this.openRegisterFormLinkLabel.Name = "openRegisterFormLinkLabel";
+            this.openRegisterFormLinkLabel.Size = new System.Drawing.Size(501, 29);
+            this.openRegisterFormLinkLabel.TabIndex = 4;
+            this.openRegisterFormLinkLabel.TabStop = true;
+            this.openRegisterFormLinkLabel.Text = "Don\'t have an Account yet? Create an Account";
+            this.openRegisterFormLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OpenRegisterFormLinkLabel_LinkClicked);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(92, 95);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(123, 29);
+            this.passwordLabel.TabIndex = 5;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(268, 92);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(356, 34);
+            this.passwordTextBox.TabIndex = 6;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(433, 175);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(129, 46);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.loginButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(675, 283);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.openRegisterFormLinkLabel);
+            this.Controls.Add(this.showPasswordCheckBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.loginButton);
+            this.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void usernameLabel_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.CheckBox showPasswordCheckBox;
+        private System.Windows.Forms.LinkLabel openRegisterFormLinkLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RegistrationForm.Connection;
+
+namespace RegistrationForm
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            usernameTextBox.Select();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OpenRegisterFormLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            RegisterForm register = new RegisterForm();
+            register.ShowDialog();
+        }
+
+        private void ShowPasswordCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(showPasswordCheckBox.Checked == true)
+            {
+                passwordTextBox.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                passwordTextBox.UseSystemPasswordChar = true; 
+            }
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(usernameTextBox.Text) && 
+                !string.IsNullOrEmpty(passwordTextBox.Text))
+            {
+                string mySQL = string.Empty;
+
+                mySQL += "SELECT * FROM User_Registration_Form";
+                mySQL += "WHERE Username = '" + usernameTextBox.Text + "' ";
+                mySQL += "AND Password = '" + passwordTextBox.Text + "' ";
+
+                DataTable userData = ServerConnection.executeSQL(mySQL);
+
+                if(userData.Rows.Count > 0)
+                {
+                    usernameTextBox.Clear();
+                    passwordTextBox.Clear();
+                    showPasswordCheckBox.Checked = false;
+
+                    this.Hide();
+
+                    MainForm formMain = new MainForm();
+                    formMain.ShowDialog();
+                    formMain = null;
+
+                    this.Show();
+                    this.usernameTextBox.Select();
+
+                }
+                else
+                {
+                    MessageBox.Show("The Username or Passowrd is Incorrect",
+                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    usernameTextBox.Focus();
+                    usernameTextBox.SelectAll();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Username and Password",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                usernameTextBox.Select();
+            }
+        }
+    }
+}
